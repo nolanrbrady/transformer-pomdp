b9ecd4a20a9213bfb972010b00380d61a651268d
Nolan Brady <nolan.brady@colorado.edu>
2025-03-31 13:04:12 -0600

    slurm scripts to train the models

---
+++ run_infini_policy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=InfiniViT_Policy             # Job name
+#SBATCH --partition=gpu-a100-40g      # Partition
+#SBATCH --mail-type=ALL                    # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=nobr3541@colorado.edu     # Where to send mail
+#SBATCH --nodes=1                            # Run all processes on a single node
+#SBATCH --ntasks=1                           # Run a single task
+#SBATCH --time=3-00:00:00                 # Time limit hrs:min:sec
+#SBATCH --output=run_policy_infini.log           # Standard output and error log
+#SBATCH --gres=gpu:1                      # request 1 gpu
+
+
+# Execute the training script
+python ppo_infini.py
